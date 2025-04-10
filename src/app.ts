b5a4cb00cb7d3bb174a005fdfdcdfa3dd2c7cd60
AKOWSPC <akows141@gmail.com>
2025-04-10 20:36:11 +0900

    Feat: 과도한 요청을 차단하는 기능 구현. (모종의 이유로 단시간에 요청이 급격하게 발생하여, 서버가 폭주하다 마비되는 상황을 방지하기 위함.) (1분 내에 30회 이상 요청이 들어올 경우, 백엔드에서 이를 차단.)

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,26 @@
 
 import express from 'express';
+import rateLimit from 'express-rate-limit';
 import userRouter from './routes/user.route';
 import pingRouter from './routes/ping.route';
 import taskRouter from './routes/task.route';
 import { errorHandler } from './middlewares/errorHandler';
 
+const RATE_LIMIT_WINDOW_MS = 1 * 60 * 1000; // 1분
+const RATE_LIMIT_MAX_CALLS = 30;
+
 const app = express();
 
 app.use(express.json());
+
+const limiter = rateLimit({
+    windowMs: RATE_LIMIT_WINDOW_MS, // 요청 제한 초기화 시간
+    max: RATE_LIMIT_MAX_CALLS, // 분당 요청 제한 수치
+    message: `해당 IP로부터 분당 ${RATE_LIMIT_MAX_CALLS}회 이상의 요청이 감지되었습니다. ${RATE_LIMIT_WINDOW_MS / 1000 / 60}분 뒤에 다시 시도해주세요.`,
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+app.use(limiter);
 
 // 테스트 API
 app.use('/api/ping', pingRouter);

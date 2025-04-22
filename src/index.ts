743f29a5acae0801a0eeafcfe4b2398795807d13
Next-dreamer <mjo2640@gmail.com>
2025-04-22 19:42:41 +0900

    chore: index.ts의 불필요한 console.log 제거

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,6 @@ dotenv.config();
 
 import app from "./app";
 
-console.log("▶︎ DB_HOST =", process.env.DB_HOST);
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`✅ Server is running on http://localhost:${PORT}`);

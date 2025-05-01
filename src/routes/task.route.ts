3104ec4a26fa4d91af6f02848762ec28a69f0fe0
AKOWSPC <akows141@gmail.com>
2025-05-01 16:22:40 +0900

    Fix: 과도한 로그인 유효성 검사 요청으로 기능이 마비되는 문제를 수정.

--- src/routes/task.route.ts
+++ src/routes/task.route.ts
@@ -12,21 +12,20 @@ import {
 import { TaskBodySchema, DayQuerySchema, WeekQuerySchema, MonthQuerySchema } from '../types/task';
 import { validateBody } from '../middlewares/validate/validateBody';
 import { validateQuery } from '../middlewares/validate/validateQuery';
-import { verifyAccessToken } from '../middlewares/login.middleware';
 
 const router = express.Router();
 
 // ✅ 일정 등록, 수정, 삭제
-router.post('/', verifyAccessToken, validateBody(TaskBodySchema), createTaskController);
-router.patch('/:id', verifyAccessToken, validateBody(TaskBodySchema), updateTaskController);
-router.delete('/:id', verifyAccessToken, deleteTaskController);
+router.post('/', validateBody(TaskBodySchema), createTaskController);
+router.patch('/:id', validateBody(TaskBodySchema), updateTaskController);
+router.delete('/:id', deleteTaskController);
 
 // ✅ 일정 경로 계산
-router.get('/:id/path', verifyAccessToken, getTaskPathController);
+router.get('/:id/path', getTaskPathController);
 
 // ✅ 일정 조회 (일간, 주간, 월간)
-router.get('/day', verifyAccessToken, validateQuery(DayQuerySchema), getTasksByDayController);
-router.get('/week', verifyAccessToken, validateQuery(WeekQuerySchema), getTasksByWeekController);
-router.get('/month', verifyAccessToken, validateQuery(MonthQuerySchema), getTasksByMonthController);
+router.get('/day', validateQuery(DayQuerySchema), getTasksByDayController);
+router.get('/week', validateQuery(WeekQuerySchema), getTasksByWeekController);
+router.get('/month', validateQuery(MonthQuerySchema), getTasksByMonthController);
 
 export default router;
